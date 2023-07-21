7b64e2cccd8bb8578986edefe1360f25259ed2ca
Ilya Shevchenko <6BLK@mail.ru>
2023-07-22 00:59:53 +0300

    Example 005 condition If-Else is added

---
+++ Lection 1/Example005_If/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите, пожалуйста, имя: ");
+string name = Console.ReadLine();
+if (name.ToLower() == "таня")
+{
+    Console.Write("Уррррааа!!!");
+}
+else
+{
+    Console.Write("Привет");
+}
