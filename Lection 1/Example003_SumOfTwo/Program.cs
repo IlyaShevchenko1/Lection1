f1a384b4c9929b76de8405defcfdd4a77c79f45e
Ilya Shevchenko <6BLK@mail.ru>
2023-07-22 00:31:57 +0300

    Add Random function in Example 003

--- Lection 1/Example003_SumOfTwo/Program.cs
+++ Lection 1/Example003_SumOfTwo/Program.cs
@@ -1,4 +1,6 @@
-﻿int num1 = 3;
-int num2 = 5;
+﻿int num1 = new Random ().Next (1 , 10); //1, 2, 3, 4, 5, 6, 7, 8, 9
+Console.WriteLine ("Первое число: " + num1);
+int num2 = new Random ().Next (1, 10);
+Console.WriteLine ("Второе число: " + num2);
 int sum = num1 + num2;
-Console.WriteLine (sum);
+Console.WriteLine ("Сумма двух чисел: " + sum);
