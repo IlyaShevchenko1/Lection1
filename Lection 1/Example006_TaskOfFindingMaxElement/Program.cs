dda17bb0010ff5205dee158ab9d74482316c79b5
Ilya Shevchenko <6BLK@mail.ru>
2023-07-22 01:18:46 +0300

    Example 6 Task of finding max weight

---
+++ Lection 1/Example006_TaskOfFindingMaxElement/Program.cs
@@ -0,0 +1,15 @@
+﻿int weight1 = 1;
+int weight2 = 2;
+int weight3 = 6;
+int weight4 = 8;
+int weight5 = 4;
+
+int max = weight1;
+
+if (weight1 > max) max = weight1;
+if (weight2 > max) max = weight2;
+if (weight3 > max) max = weight3;
+if (weight4 > max) max = weight4;
+if (weight5 > max) max = weight5;
+
+Console.WriteLine ("Вес самого тяжелого груза " + max);
